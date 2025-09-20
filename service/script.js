1f891e211f093f740369e2d9dff6e014b62e3bce
Madaminbek <abduhoshimov1995@gmail.com>
2025-09-20 16:32:06 +0500

    togirlandi

--- service/script.js
+++ service/script.js
@@ -1,6 +1,17 @@
 function ikitataHarf(str) {
-    const
-    for (let i = 0; index < str.length; i++) {
-        if(s)
+  let bormi = false;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        console.log("Ikkita bir hil harf bu =", str[i]);
+        bormi = true;
+      }
     }
+  }
+  if (!bormi) {
+    console.log(null);
+  }
 }
+ikitataHarf("Javascript");
+ikitataHarf("Frontend");
+ikitataHarf("Python");
